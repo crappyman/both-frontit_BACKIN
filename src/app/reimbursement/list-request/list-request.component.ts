548353fc09b3a6cf11e92c6bd3936d1a8933f99e
Rana.ismael <rana4ismael@gmail.com>
2022-03-10 08:00:53 -0500

    fir

---
+++ src/app/reimbursement/list-request/list-request.component.ts
@@ -0,0 +1,89 @@
+import { Component, OnInit } from '@angular/core';
+import { RequestService } from '../request.service';
+import { Request } from '../request.model';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/user/auth.service';
+import { ConnectableObservable } from 'rxjs';
+
+@Component({
+  selector: 'app-list-request',
+  templateUrl: './list-request.component.html',
+  styleUrls: ['./list-request.component.css']
+})
+export class ListRequestComponent implements OnInit {
+
+  allRequests: Request[] = [];
+
+  togglePReq: boolean = false;
+  toggleRequests: boolean = false;
+  
+  newRequest: Request = {
+      reqId: 0,
+      userId: 0,
+      reqType: '',
+      reqAmount: 0,
+      reqStatus: '',
+      submitDate: '',
+      approveDate: '',
+      manager: ''
+    };
+  
+  constructor(private requestService: RequestService,
+              private router: Router,
+              private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.loadAllRequests();
+  }
+
+  togglePending() {
+    if(this.togglePReq) {
+      this.togglePReq = false;
+    } else {
+      this.togglePReq = true;
+    }
+  }
+
+  toggleAllRequests() {
+    if(this.toggleRequests) {
+      this.toggleRequests = false;
+    } else {
+      this.toggleRequests = true;
+    }
+  }
+
+  loadAllRequests() {
+    //this has current manager info that logged in
+    let currentManager: any = this.authService.retrieveUser();
+    console.log(currentManager);
+    
+    //get all requests from the backend and stored in response
+    this.requestService.viewAllRequest().subscribe((response) => {
+      console.log(response); // a list of all requests
+      for(let i = 0; i < response.length; i++) {
+        if(response[i].manager == currentManager.userID) {
+          console.log(this.allRequests[i]);
+          this.allRequests.push(response[i]);
+        }  
+      }
+    });
+  }
+
+  // addRequest() {
+  //   this.requestService.addRequest(this.newRequest).subscribe((response) => {
+  //     console.log(response);
+  //     this.newRequest = {
+  //       reqId: 0,
+  //       userId: 0,
+  //       reqType: '',
+  //       reqAmount: 0,
+  //       reqStatus: '',
+  //       submitDate: '',
+  //       approvedDate: '',
+  //       manager: ''
+  //     };
+
+  //     this.loadAllRequests();
+  //   });
+  // }
+}
---
+++ src/app/user/ep-request/ep-request.component.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RequestService } from 'src/app/reimbursement/request.service';
+import { Request } from 'src/app/reimbursement/request.model';
+import { Account } from 'src/app/account/account.model';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-ep-request',
+  templateUrl: './ep-request.component.html',
+  styleUrls: ['./ep-request.component.css']
+})
+export class EpRequestComponent implements OnInit {
+
+  allEpRequests: Request[] = [];
+  toggleAdd: boolean = false;
+
+  newAccount: Account = {
+    userID: 0,
+    username: "",
+    password: "",
+    fullName: "",
+    email: "",
+    role_id: 0,
+    role: ""
+  }
+  
+  newRequest: Request = {
+    reqId: 0,
+    userId: 0,
+    reqType: '',
+    reqAmount: 0,
+    reqStatus: '',
+    submitDate: '',
+    approveDate: '',
+    manager: ''
+  };
+
+  constructor(private requestService: RequestService,
+              private router: Router,
+              private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.loadAllRequests();
+  }
+
+  toggleAddForm() {
+    if(this.toggleAdd) {
+      this.toggleAdd = false;
+    } else {
+      this.toggleAdd = true;
+    }
+  }
+
+  loadAllRequests() {
+    let currentEmployee: any = this.authService.retrieveUser();
+    this.requestService.viewAllRequest().subscribe((response) => {
+      console.log(response);
+
+      for(let i = 0; i < response.length; i++) {
+        if(response[i].userId == currentEmployee.userID) {
+          this.allEpRequests.push(response[i]);
+        }
+      }
+    });
+  }
+
+  addRequest() {
+    //let currentUser: any = this.authService.retrieveUser();
+    //console.log(currentUser);
+    this.requestService.addRequest(this.newRequest).subscribe((response) => {
+      console.log(response);
+      this.newRequest = {
+        reqId: 0,
+        userId: 0,
+        reqType: '',
+        reqAmount: 0,
+        reqStatus: '',
+        submitDate: '',
+        approveDate: '',
+        manager: ''
+      };
+
+      this.loadAllRequests();
+    });
+  }
+}
